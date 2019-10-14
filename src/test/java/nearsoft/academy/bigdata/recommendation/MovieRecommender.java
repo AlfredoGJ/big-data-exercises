d329229016a47492cc30cb842e7545fc122346c6
Alfredo <josalfk@hotmail.com>
2019-10-14 12:28:50 -0500

    Comments out in addPreference function

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -83,7 +83,7 @@ public class MovieRecommender {
     }
 
     void addPreference(String userID, String productID, float score) {
-        
+
         reviewCount++;
         if (!userIDs.containsKey(userID))
             userIDs.put(userID, ++userCount);
@@ -97,17 +97,7 @@ public class MovieRecommender {
 
         addPreferenceToUser((GenericUserPreferenceArray) usersPreferences.get(longUserID),new GenericPreference(longUserID, longProductID, score));
 
-        // GenericUserPreferenceArray preferencesArray = (GenericUserPreferenceArray) usersPreferences.get(longUserID);
-        // ArrayList preferencesList = new ArrayList();
-
-        // if (preferencesArray != null) {
-        //     for (Integer i = 0; i < preferencesArray.length(); i++)
-        //         preferencesList.add(preferencesArray.get(i));
-        // }
-
-        // preferencesList.add(new GenericPreference(longUserID, longProductID, score));
-
-        // usersPreferences.put(longUserID, new GenericUserPreferenceArray(preferencesList));
+        
 
     }
     void addPreferenceToUser(GenericUserPreferenceArray existentPreferences, GenericPreference newPreference){
